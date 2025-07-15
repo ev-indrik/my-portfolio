db3218247a7640ed6680bc4d6a6963c598dc3d0a
ev-indrik <delfinmur@gmail.com>
2025-07-15 13:19:03 +0300

    refactor: simplify Router logic to one conditional layout

--- src/layuot/WebsiteLayout.tsx
+++ src/layuot/WebsiteLayout.tsx
@@ -1,16 +1,21 @@
 import {type FC} from 'react';
 import Header from "../components/header/Header.tsx";
 import Footer from '../components/footer/Footer.tsx';
-import {Outlet} from "react-router";
+import {Outlet, useLocation} from "react-router";
 
 const WebsiteLayout: FC = () => {
+    const location = useLocation();
+    const isHome = location.pathname === '/home';
+
     return (
         <div className={'layout-wrapper'}>
-            <Header/>
+            <Header />
+
             <div className={'pages-wrapper'}>
-                <Outlet/>
+                <Outlet />
             </div>
-            <Footer/>
+
+            {!isHome && <Footer />}
         </div>
     );
 };
