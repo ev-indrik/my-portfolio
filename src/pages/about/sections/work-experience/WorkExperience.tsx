fa833aa24ceee91c148bca123398f7c868d1a6ba
ev-indrik <delfinmur@gmail.com>
2025-07-15 21:16:42 +0300

    WIP: WorkExperience component

--- src/pages/about/sections/work-experience/WorkExperience.tsx
+++ src/pages/about/sections/work-experience/WorkExperience.tsx
@@ -4,18 +4,71 @@ import './WorkExperience.scss'
 import bgLeaves from './images/bg-leaves.png'
 import titleImg from "@/pages/about/sections/work-experience/images/clock-img.png";
 import TitleIconGroup from "@/components/title-icon-group/TitleIconGroup";
+import WebsiteTypography from "@/components/website-typography/WebsiteTypography";
+
+const {Title, Paragraph} = WebsiteTypography;
 
 const WorkExperience: FC = () => {
     return (
         <div className={'work-experience-wrapper'}>
-
-            <div className={'bg-img-wrapper'}>
-                <img src={bgLeaves} alt={"background illustration"}/>
-            </div>
-
             <div className={'container'}>
 
-                <TitleIconGroup content={{ title: 'My Work Experience', image: titleImg }} marginBottom={'-30px'} />
+                <div className={'bg-img-wrapper'}>
+                    <img src={bgLeaves} alt={"background illustration"}/>
+                </div>
+
+                <TitleIconGroup content={{title: 'My Work Experience', image: titleImg}} marginBottom={'-30px'}/>
+
+                <div className={'experiences-box'}>
+                    <div className={'experience-item-wrapper'}>
+                       <div className={'experience-content'}>
+                           <Title level={3}>
+                               {'2024 – now'}
+                           </Title>
+
+                           <Paragraph className={'paragraph-wrapper'}>{'Frontend Developer (trainee)\n' +
+                               'Pritster Holdings (Pty) Ltd t/a Groundwork Solutions'}</Paragraph>
+                       </div>
+                    </div>
+
+                    <div className={'experience-item-wrapper even'}>
+                        <div className={'experience-content even-content'}>
+                            <Title level={3}>
+                                {'2024 – now'}
+                            </Title>
+
+                            <Paragraph className={'paragraph-wrapper'}>{'Frontend Developer (trainee)\n' +
+                                'Pritster Holdings (Pty) Ltd t/a Groundwork Solutions'}</Paragraph>
+                        </div>
+                    </div>
+
+                    <div className={'experience-item-wrapper'}>
+                        <div className={'experience-content'}>
+                            <Title level={3}>
+                                {'2024 – now'}
+                            </Title>
+
+                            <Paragraph className={'paragraph-wrapper'}>{'Frontend Developer (trainee)\n' +
+                                'Pritster Holdings (Pty) Ltd t/a Groundwork Solutions'}</Paragraph>
+                        </div>
+                    </div>
+
+                    <div className={'experience-item-wrapper even'}>
+                        <div className={'experience-content even-content'}>
+                            <Title level={3}>
+                                {'2024 – now'}
+                            </Title>
+
+                            <Paragraph
+                                className={'paragraph-wrapper'}
+                            >
+                                {'Frontend Developer (trainee)\n' +
+                                'Pritster Holdings (Pty) Ltd t/a Groundwork Solutions'}
+                            </Paragraph>
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
         </div>
