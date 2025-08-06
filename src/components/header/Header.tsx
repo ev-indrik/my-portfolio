7ba44cf0c16641bbbb1bab105e7937b42d36b2a5
ev-indrik <delfinmur@gmail.com>
2025-08-06 19:15:33 +0300

    feat: add header burger

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,17 @@
 import {type FC, useEffect, useState} from 'react';
-import {Button, Col, Menu, type MenuProps, Row} from "antd";
+import {Button, Col, Menu, type MenuProps, Popover, Row } from "antd";
 
 import {useLocation, useNavigate} from "react-router";
 import MenuItem from './menu-item/MenuItem';
 import {SvgIcon} from "../icon/SvgIcon.tsx";
 
 import greenBg from '../../assets/green-bg-logo.png'
+import popoverBgImg from './images/popover-bg-img.png'
 
 import './Header.scss'
+import useBreakpoint from "antd/es/grid/hooks/useBreakpoint";
+import WebsiteButton from "@/components/website-button/WebsiteButton";
+import WebsiteDivider from "@/components/website-divider/WebsiteDivider";
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -31,14 +35,19 @@ const items: MenuItem[] = [
 ]
 
 // ===>> popup for Dark&Light modes, eng&ua
-// const content = (
+// const switchContent = (
 //     <Space direction={'vertical'}>
 //         <Button type={'default'} block>{'Go to Ukrainian'}</Button>
 //         <Button type={'default'} block>{'Go to the Dark side'}</Button>
 //     </Space>
 // )
 
+
 const Header: FC = () => {
+
+    const {xxl, xl, lg} = useBreakpoint()
+
+    const [isPopoverOpen, setPopoverOpen] = useState(false)
 
     const navigate = useNavigate()
     const {pathname} = useLocation()
@@ -50,7 +59,7 @@ const Header: FC = () => {
 
         setCurrent(currentLocation)
 
-        if(!currentLocation){
+        if (!currentLocation) {
             setCurrent('home')
         }
 
@@ -61,46 +70,94 @@ const Header: FC = () => {
         navigate(`/${e.key}`)
     };
 
-    const onLogoClick = ()=>{
+    const onLogoClick = () => {
         navigate('/home')
     }
+
+    const onOpenChange = (isOpen: boolean) => {
+        setPopoverOpen(isOpen)
+    }
+
+    const menuContent = (
+        <div className={'popup-wrapper'}>
+
+            <div className={'popover-bg-wrapper'}>
+                <img src={popoverBgImg} alt={"illustration for background"}/>
+            </div>
+
+                <Menu
+                    className={'header-menu'}
+                    onClick={onClick}
+                    selectedKeys={[current]}
+                    mode={"vertical"}
+                    items={items}
+                    overflowedIndicator={false}
+                />
+
+            <WebsiteDivider paddingTop={5} paddingBottom={5}/>
+        </div>
+    )
 
     return (
         <div className={'header-wrapper'}>
             <div className={'container'}>
+                {(xxl || xl || lg) ? (
 
-                <Row justify={'space-between'}>
-                    <Col>
-                        <Button type={'text'} className={'logo-btn'} onClick={onLogoClick}>
-                            <SvgIcon type={'main-whale'} />
-                        </Button>
-                        <div className={'header-logo-bg'}>
-                            <img src={greenBg} alt={"Green background"}/>
-                        </div>
-                    </Col>
+                        <Row justify={'space-between'}>
+                            <Col>
+                                <Button type={'text'} className={'logo-btn'} onClick={onLogoClick}>
+                                    <SvgIcon type={'main-whale'}/>
+                                </Button>
+                                <div className={'header-logo-bg'}>
+                                    <img src={greenBg} alt={"Green background"}/>
+                                </div>
+                            </Col>
 
-                    <Col>
-                        <Row justify={'end'} align={'middle'} style={{ paddingTop: 24}}>
-                            <Menu
-                                className={'header-menu'}
-                                onClick={onClick}
-                                selectedKeys={[current]}
-                                mode={"horizontal"}
-                                items={items}
-                                overflowedIndicator={false}
-                            />
-                            {/*===>> popup for Dark&Light modes, eng&ua*/}
-                            {/*<Popover content={content} placement={'bottomRight'} arrow={false}>*/}
-                            {/*    <Button type={'text'}>*/}
-                            {/*        <SvgIcon type={'burger'}/>*/}
-                            {/*    </Button>*/}
-                            {/*</Popover>*/}
+                            <Col>
+                                <Row justify={'end'} align={'middle'} style={{paddingTop: 24}}>
+                                    <Menu
+                                        className={'header-menu'}
+                                        onClick={onClick}
+                                        selectedKeys={[current]}
+                                        mode={"horizontal"}
+                                        items={items}
+                                        overflowedIndicator={false}
+                                    />
+                                    {/*===>> popup for Dark&Light modes, eng&ua*/}
+                                    {/*<Popover content={switchContent} placement={'bottomRight'} arrow={false}>*/}
+                                    {/*    <Button type={'text'}>*/}
+                                    {/*        <SvgIcon type={'burger'}/>*/}
+                                    {/*    </Button>*/}
+                                    {/*</Popover>*/}
+                                </Row>
+                            </Col>
                         </Row>
-                    </Col>
-                </Row>
+                    )
+                    :
+                    (
+                        <Row justify={'end'}>
+                            <Popover
+                                title={null}
+                                content={menuContent}
+                                trigger={"click"}
+                                showArrow={false}
+                                onOpenChange={onOpenChange}
+                                open={isPopoverOpen}
+                            >
+                                <WebsiteButton
+                                    className={"burger-button"}
+                                    btnType={"icon"}
+                                    icon={<SvgIcon
+                                        type={isPopoverOpen ? "refresh" : "burger"}/>}
+                                />
+                            </Popover>
+                        </Row>
+                    )}
+
             </div>
         </div>
-    );
+    )
+        ;
 };
 
 export default Header;
