5789af384c934315185af213a894d232734a1023
ev-indrik <delfinmur@gmail.com>
2025-10-04 23:27:43 +0300

    feat: add responsiveness to ContactsBlock

--- src/pages/contacts/sections/contacs-block/ContactsBlock.tsx
+++ src/pages/contacts/sections/contacs-block/ContactsBlock.tsx
@@ -9,11 +9,12 @@ import type {ContactItemType} from "@/pages/contacts/sections/contacs-block/cont
 
 import geoImg from './images/contacts-icons/geo-icon.png'
 import emailImg from './images/contacts-icons/email-icon.png'
-import phoneImg from './images/contacts-icons/phone-icon.png'
+// import phoneImg from './images/contacts-icons/phone-icon.png'
 import tImg from './images/contacts-icons/t-icon.png'
 import viberImg from './images/contacts-icons/viber-icon.png'
 import SocialButtons from "@/components/social-buttons/SocialButtons";
 import WebsiteDivider from "@/components/website-divider/WebsiteDivider";
+import useBreakpoint from "antd/es/grid/hooks/useBreakpoint";
 
 const contactsBlockContent: ContactItemType[] = [
     {
@@ -21,15 +22,14 @@ const contactsBlockContent: ContactItemType[] = [
         title: 'Location',
         description: 'Kyiv, Ukraine'
     },
-
-    {
-        imgUrl: phoneImg,
-        title: 'Mobile Phone',
-        description: '+380 68 355 58 48'
-    },
+    // {
+    //     imgUrl: phoneImg,
+    //     title: 'Mobile Phone',
+    //     description: '+380 68 355 58 48'
+    // },
     {
         imgUrl: viberImg,
-        title: 'Viber',
+        title: 'Viber/Phone',
         description: '+380 68 355 58 48'
     },
     {
@@ -45,6 +45,9 @@ const contactsBlockContent: ContactItemType[] = [
 ]
 
 const ContactsBlock: FC = () => {
+
+    const {xxl, xl, lg } = useBreakpoint()
+
     return (
         <div className={'contacts-block-wrapper'}>
 
@@ -54,10 +57,18 @@ const ContactsBlock: FC = () => {
             <div className={'container'}>
 
                 <Row justify={'center'}>
-                    <Col span={14}>
+                    <Col
+                        span={14}
+                        xxl={14}
+                        xl={14}
+                        lg={14}
+                        md={18}
+                        sm={20}
+                        xs={24}
+                    >
 
                         <Row justify={'center'} className={'contacts-items-wrapper'}>
-                            <Col span={24} style={{paddingLeft: 80}}>
+                            <Col span={24} style={{paddingLeft: (xxl||xl||lg) ? 80 : 10}}>
 
                                 {contactsBlockContent.map((it, index) => (
                                     <ContactItem content={it} key={index}/>
@@ -66,12 +77,19 @@ const ContactsBlock: FC = () => {
                         </Row>
 
                         <Row justify={'center'}>
-                            <Col span={8}>
+                            <Col
+                                span={8}
+                                xxl={8}
+                                xl={8}
+                                lg={8}
+                                md={10}
+                                sm={14}
+                                xs={18}
+                            >
                                 <SocialButtons color={'primary'}/>
                             </Col>
                         </Row>
                     </Col>
-
                 </Row>
 
                 <WebsiteDivider paddingBottom={50} paddingTop={50} />
